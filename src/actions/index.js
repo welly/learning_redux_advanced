d0ddda8374cdf5100b399036d2b82d51527e7e99
Alastair Moore <alastair@fatbeehive.com>
2017-11-10 14:03:18 +0000

    Added openweather url

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 const API_KEY = '0b6f8ccfb8bb5ea14fc23f6f08eac5cf';
+const URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
