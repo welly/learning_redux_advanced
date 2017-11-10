950e723b53b9f3f798a82b4069d8d23b06e550b6
Alastair Moore <alastair@fatbeehive.com>
2017-11-10 14:08:42 +0000

    Imported axios library and made call to url

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,17 @@
+import axios from 'axios';
+
 const API_KEY = '0b6f8ccfb8bb5ea14fc23f6f08eac5cf';
-const URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
-export function fetchWeather() {
+export function fetchWeather(city) {
+
+	const URL = `${ROOT_URL}&q=${city},GB`;
+	const request = axios.get(URL);
+
 	return {
-		type: FETCH_WEATHER
+		type: FETCH_WEATHER,
+		payload: request
 	};
 }
