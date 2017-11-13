bb835d8e77fcf7fbe999f0375e8e67926ab3c23e
Alastair Moore <welly@protonmail.com>
2017-11-13 23:34:55 +0000

    Imported google map component and output to table, passing lat, lon props

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
 
@@ -9,21 +10,14 @@ class WeatherList extends Component {
 		const temps = cityData.list.map(weather => weather.main.temp);
 		const pressures = cityData.list.map(weather => weather.main.pressure);
 		const humidities = cityData.list.map(weather => weather.main.humidity);
+		const {lat, lon} = cityData.city.coord;
 
 		return (
 			<tr key={name}>
-				<td>
-					{name} 
-				</td>
-				<td>
-					<Chart data={temps} colour="red" />
-				</td>
-				<td>
-					<Chart data={pressures} colour="green" />
-				</td>
-				<td>
-					<Chart data={humidities} colour="blue" />
-				</td>
+				<td><GoogleMap lat={lat} lon={lon} /></td>
+				<td><Chart data={temps} colour="red" /></td>
+				<td><Chart data={pressures} colour="green" /></td>
+				<td><Chart data={humidities} colour="blue" /></td>
 			</tr>
 		);
 	}
