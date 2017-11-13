ca1c73ba2f076ac384dd3fa8f7f7c0b5ee232e2a
Alastair Moore <welly@protonmail.com>
2017-11-13 23:23:48 +0000

    Padded out columns to display data

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Chart from '../components/chart';
 
 class WeatherList extends Component {
 
 	renderWeather(cityData) {
 		const name = cityData.city.name
+		const temps = cityData.list.map(weather => weather.main.temp);
+		const pressures = cityData.list.map(weather => weather.main.pressure);
+		const humidities = cityData.list.map(weather => weather.main.humidity);
+
 		return (
 			<tr key={name}>
-				<td>{name}</td>
+				<td>
+					{name} 
+				</td>
+				<td>
+					<Chart data={temps} colour="red" />
+				</td>
+				<td>
+					<Chart data={pressures} colour="green" />
+				</td>
+				<td>
+					<Chart data={humidities} colour="blue" />
+				</td>
 			</tr>
 		);
 	}
